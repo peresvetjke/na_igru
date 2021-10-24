2998e6b99cd66fd408c0e2c5332b820c7926e0e7
peresvetjke <igor.udalov@gmail.com>
2021-10-24 11:09:25 +0300

    Add corrections to locations form_inline

--- app/assets/utilities/form_inline.js
+++ app/assets/utilities/form_inline.js
@@ -5,22 +5,32 @@ document.addEventListener("turbolinks:load", () => {
   var cancel_controls = $(".form-inline-cancel");
   var errors = $(".errors")[0]  
 
-  if (errors) {
-    var resourceId = $(".errors")[0].dataset.resourceId;
-    formInlineHandler(resourceId);
-  }
 
+
+  console.log('edit_controls.length is ' + edit_controls.length)
   if (edit_controls.length) {
     for (var i = 0; i < edit_controls.length; i++) {
       edit_controls[i].addEventListener('click', formInlineLinkHandler)
       cancel_controls[i].addEventListener('click', formInlineLinkHandler)
+      
+      // assignResourceIdToRowsClassList ; 
+      var cancelControl = cancel_controls[i]
+      var resourceId = $(cancelControl).data("id")
+      var formInlineRow = $(cancelControl).parents("tr")[0]
+      $(formInlineRow).attr("data-id", resourceId);
     }
   }
+
+  if (errors) {
+    var resourceId = $(".errors")[0].dataset.id;
+    formInlineHandler(resourceId);
+  }
+  
 })
 
 function formInlineLinkHandler(event) {
   event.preventDefault();
-  var resourceId = this.dataset.resourceId;
+  var resourceId = this.dataset.id;
   
   formInlineHandler(resourceId);
 }
@@ -28,10 +38,11 @@ function formInlineLinkHandler(event) {
 
 function formInlineHandler(resourceId) {
   var currentRow = $(".form-inline-row-current").filter(function() { 
-    return $(this).data("resource-id") == resourceId;
+    // return $(this).children("tr").data("id") == resourceId;
+    return $(this).data("id") == resourceId;
   });
   var formInlineRow = $(".form-inline-row-edit").filter(function() { 
-    return $(this).data("resource-id") == resourceId;
+    return $(this).data("id") == resourceId;
   });
 
   currentRow.toggle();
